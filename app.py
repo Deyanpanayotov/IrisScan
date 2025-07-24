cba5adc3ac6adb4f4e62e8ceb757cc4938ed2414
Deyanpanayotov <61658611+Deyanpanayotov@users.noreply.github.com>
2025-07-24 14:22:25 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -24,3 +24,7 @@ def analyze():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+@app.route('/')
+def home():
+    return "🧠 IrisScan API is working!"
